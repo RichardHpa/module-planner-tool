b1bfd2abdc5a9f9ad3807feea8382ac545599357
RichardHpa <richard.m.hpa@gmail.com>
2020-02-05 13:35:20 +1300

    classes get saved into localstorage, and autosave every 5 minutes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 $( function() {
 
-    const classesNum = 50;
-    let temp = $('#classCardTemplate');
-    for (var i = 1; i <= classesNum; i++) {
-        let temp = $('#classCardTemplate').clone();
-        var node = temp.prop('content');
-        var content = $(node).find('li');
-
-        content.find('.card').text('Class ' + (i));
-
-
-        $('#classList').append(content);
+    if(localStorage.items){
+        const items = JSON.parse(localStorage.items)
+        $('#classList').html(items);
+    } else {
+        const classesNum = 50;
+        let temp = $('#classCardTemplate');
+        for (var i = 1; i <= classesNum; i++) {
+            let temp = $('#classCardTemplate').clone();
+            var node = temp.prop('content');
+            var content = $(node).find('li');
+            content.find('.card').text('Class ' + (i));
+            $('#classList').append(content);
+        }
+        temp.remove();
     }
     $('#classList').sortable({
         items: "li:not(.workshop)",
@@ -20,7 +23,22 @@ $( function() {
 
     $('.card').dblclick(function(){
         const newTitle = prompt('New Topic');
+        if (newTitle === null) {
+            return; //break out of the function early
+        }
         $(this).text(newTitle)
-        console.log();
+    });
+
+    setInterval(function(){
+        const items = JSON.stringify($('#classList').html());
+        localStorage.setItem('items', items);
+    }, 300000);
+    //autosave every 5 minutes
+
+
+    $('#saveBtn').click(function(e){
+        e.preventDefault();
+        const items = JSON.stringify($('#classList').html());
+        localStorage.setItem('items', items);
     });
 } );
